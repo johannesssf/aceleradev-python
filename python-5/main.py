5b7e551bc473ece53d0bf546ebf7142e109e3ada
Johannes Ferreira <johannesssf@gmail.com>
2020-05-15 14:28:01 -0300

    First module challenge - final version

--- python-5/main.py
+++ python-5/main.py
@@ -16,8 +16,16 @@ records = [
 ]
 
 
-def calc_minutes_in_period(start, end):
-    """Calculates minutes
+def calc_diurnal_minutes(start, end):
+    """Calculates how many minutes occurred in the diurnal period in a
+    phonecall. The diurnal period is between 6:00 and 22:00.
+
+    Arguments:
+        start {int} -- Starting timestamp
+        end {int} -- Ending timestamp
+
+    Returns:
+        int -- minutes in diurnal period
     """
     call_start = datetime.fromtimestamp(start)
     call_end = datetime.fromtimestamp(end)
@@ -29,52 +37,53 @@ def calc_minutes_in_period(start, end):
                         call_start.month,
                         call_start.day, 22, 0, 0)
 
-    MINUTES_16HOURS = 16 * 60
-    noct_minutes = 0
-    diur_minutes = 0
+    MINUTES_IN_16HOURS = 16 * 60
+    minutes = 0
 
     # case 1: call started and finished between 0h and 6h
     if (0 <= call_start.hour < 6) and (0 <= call_end.hour < 6):
-        diff = call_end - call_start
-        noct_minutes = diff.seconds // 60
-        return noct_minutes, diur_minutes
+        return minutes
 
     # case 2: call started and finished between 6h and 22h
     if (6 <= call_start.hour < 22) and (6 <= call_end.hour < 22):
         diff = call_end - call_start
-        diur_minutes = diff.seconds // 60
-        return noct_minutes, diur_minutes
+        minutes = diff.seconds // 60
+        return minutes
 
     # case 3: call started and finished between 22h and 24h
     if (22 <= call_start.hour < 24) and (22 <= call_end.hour < 24):
-        diff = call_end - call_start
-        noct_minutes = diff.seconds // 60
-        return noct_minutes, diur_minutes
+        return minutes
 
     # case 4: call started before 6h and finished between 6h and 21h
     if (call_start.hour < 6) and (6 <= call_end.hour < 22):
-        noct_minutes = (MARK_06H - call_start).seconds // 60
-        diur_minutes = (call_end - MARK_06H).seconds // 60
-        return noct_minutes, diur_minutes
+        minutes = (call_end - MARK_06H).seconds // 60
+        return minutes
 
     # case 5: call started between 6h and 22 and finished after 22h
     if (6 <= call_start.hour < 22) and (22 <= call_end.hour):
-        noct_minutes = (MARK_22H - call_start).seconds // 60
-        diur_minutes = (call_end - MARK_22H).seconds // 60
-        return noct_minutes, diur_minutes
+        minutes = (call_end - MARK_22H).seconds // 60
+        return minutes
 
     # case 6: call started before 6h and finished after 22h
     if (call_start.hour < 6) and (22 <= call_end.hour):
-        noct_minutes = (MARK_06H - call_start).seconds // 60
-        noct_minutes += (call_end - MARK_22H).seconds // 60
-        diur_minutes = MINUTES_16HOURS
-        return noct_minutes, diur_minutes
+        return MINUTES_IN_16HOURS
 
     # if we get here, probably it's an invalid record
-    return noct_minutes, diur_minutes
+    return minutes
 
 
-def order_bill_by_value_desc(bill):
+def order_bills_by_value_desc(bill):
+    """Orders a list of bills by its totals in descending order.
+
+    The elements of the list are dics in the following form:
+    {'source': <source phone number>, 'total': <value>}
+
+    Arguments:
+        bill {list} -- Unordered list
+
+    Returns:
+        list -- Ordered list
+    """
     for i in range(len(bill)-1):
         for j in range(i+1, len(bill)):
             if bill[j]['total'] > bill[i]['total']:
@@ -83,31 +92,50 @@ def order_bill_by_value_desc(bill):
     return bill
 
 
-def calc_fare(minutes):
-    """Calcultes the fare according to call duration.
+def calculate_bills(records):
+    """Calculates the total expense for each source phone number.
 
-    We have a fixed value for all calls and an extra value for each
-    minute.
+    Each call has a fixed value of 0.36 for the call connection and an
+    extra fare of 0.09 for each minute during the diurnal period
+    comprehended from 6:00 to 22:00.
+
+    For example:
+    call started at 01/01/2000 5:55
+    call ended at 01/01/2000 6:05 (5 minutes)
+    total: 0.36 (fixed value) + 5 (diurnal) * 0.09 (extra fare)
+
+    Arguments:
+        records {list} -- Phone call records
+
+    Returns:
+        list -- Calculated bills ordered by values descending
     """
-    total = 0.36 + (minutes * 0.09)
-    return total
+    bills = {}
+    for record in records:
+        minutes = calc_diurnal_minutes(record['start'], record['end'])
+        total = 0.36 + (minutes * 0.09)
+
+        if record['source'] in bills:
+            bills[record['source']] = bills[record['source']] + total
+        else:
+            bills[record['source']] = total
+
+    bills = [{'source': key, 'total': round(val, 2)}
+             for key, val in bills.items()]
+
+    return order_bills_by_value_desc(bills)
 
 
 def classify_by_phone_number(records):
-    bill = {}
-    for record in records:
-        noct, diur = calc_minutes_in_period(record['start'], record['end'])
-        total = calc_fare(diur)
+    """Calculates and classify a list of phone calls.
 
-        if record['source'] in bill:
-            bill[record['source']] = bill[record['source']] + total
-        else:
-            bill[record['source']] = total
+    Arguments:
+        records {list} -- Phone call records list
 
-    bill = [{'source': key, 'total': round(val, 2)}
-            for key, val in bill.items()]
-
-    return order_bill_by_value_desc(bill)
+    Returns:
+        list -- List with the calculated totals
+    """
+    return calculate_bills(records)
 
 
 if __name__ == "__main__":
--- python-5/test_main.py
+++ python-5/test_main.py
@@ -1,7 +1,12 @@
 from datetime import datetime
 
-from main import classify_by_phone_number, records, order_bill_by_value_desc, \
-                 calc_minutes_in_period, calc_fare
+from main import (
+    calc_diurnal_minutes,
+    calculate_bills,
+    classify_by_phone_number,
+    order_bills_by_value_desc,
+    records,
+)
 
 
 class TestChallenge1:
@@ -10,7 +15,7 @@ class TestChallenge1:
         result = classify_by_phone_number(records)
         assert len(result) == 6
 
-    def test_order_bill_by_value_desc(self):
+    def test_order_bills_by_value_desc(self):
         unordered = [
             {'total': 1.2},
             {'total': 2.2},
@@ -25,7 +30,7 @@ class TestChallenge1:
             {'total': 1.2},
             {'total': 1.1},
         ]
-        resutl = order_bill_by_value_desc(unordered)
+        resutl = order_bills_by_value_desc(unordered)
         assert resutl == ordered
 
     def test_calc_minutes_in_period_nocturne_or_diurne(self):
@@ -35,7 +40,6 @@ class TestChallenge1:
                 'destination': '48-666666666',
                 'end': 1546314000,
                 'start': 1546313400,
-                'nocturne': 10,
                 'diurne': 0,
             },
             {
@@ -43,7 +47,6 @@ class TestChallenge1:
                 'destination': '41-886383097',
                 'end': 1546322455,
                 'start': 1546322405,
-                'nocturne': 0,
                 'diurne': 0,
             },
             {
@@ -51,7 +54,6 @@ class TestChallenge1:
                 'destination': '41-886383097',
                 'end': 1546330255,
                 'start': 1546329600,
-                'nocturne': 0,
                 'diurne': 10,
             },
             {
@@ -59,7 +61,6 @@ class TestChallenge1:
                 'destination': '41-886383097',
                 'end': 1546359005,
                 'start': 1546355718,
-                'nocturne': 0,
                 'diurne': 54,
             },
             {
@@ -67,7 +68,6 @@ class TestChallenge1:
                 'destination': '41-886383097',
                 'end': 1546394399,
                 'start': 1546387200,
-                'nocturne': 119,
                 'diurne' : 0,
             },
             {
@@ -75,15 +75,13 @@ class TestChallenge1:
                 'destination': '41-886383097',
                 'end': 1546393679,
                 'start': 1546392845,
-                'nocturne': 13,
                 'diurne': 0,
             },
         ]
 
         for rec in local_records:
-            noc_min, diu_min = calc_minutes_in_period(rec['start'], rec['end'])
-            assert noc_min == rec['nocturne']
-            assert diu_min == rec['diurne']
+            minutes = calc_diurnal_minutes(rec['start'], rec['end'])
+            assert minutes == rec['diurne']
 
     def test_calc_minutes_in_period_nocturne_and_diurne(self):
         local_records = [
@@ -92,7 +90,6 @@ class TestChallenge1:
                 'destination': '48-666666666',
                 'end': 1546330439,
                 'start': 1546329315,
-                'nocturne': 4,
                 'diurne': 13,
             },
             {
@@ -100,15 +97,13 @@ class TestChallenge1:
                 'destination': '48-666666666',
                 'end': 1546331400,
                 'start': 1546327800,
-                'nocturne': 30,
                 'diurne': 30,
             },
         ]
 
         for rec in local_records:
-            noc_min, diu_min = calc_minutes_in_period(rec['start'], rec['end'])
-            assert noc_min == rec['nocturne']
-            assert diu_min == rec['diurne']
+            minutes = calc_diurnal_minutes(rec['start'], rec['end'])
+            assert minutes == rec['diurne']
 
     def test_calc_minutes_in_period_diurne_and_nocturne(self):
         local_records = [
@@ -117,7 +112,6 @@ class TestChallenge1:
                 'destination': '48-666666666',
                 'end': 1546387260,
                 'start': 1546387140,
-                'nocturne': 1,
                 'diurne': 1,
             },
             {
@@ -125,15 +119,13 @@ class TestChallenge1:
                 'destination': '48-666666666',
                 'end': 1546389000,
                 'start': 1546385400,
-                'nocturne': 30,
                 'diurne': 30,
             },
         ]
 
         for rec in local_records:
-            noc_min, diu_min = calc_minutes_in_period(rec['start'], rec['end'])
-            assert noc_min == rec['nocturne']
-            assert diu_min == rec['diurne']
+            minutes = calc_diurnal_minutes(rec['start'], rec['end'])
+            assert minutes == rec['diurne']
 
     def test_calc_minutes_in_period_nocturne_and_nocturne(self):
         local_records = [
@@ -142,7 +134,6 @@ class TestChallenge1:
                 'destination': '48-666666666',
                 'end': 1546387500,
                 'start': 1546329300,
-                'nocturne': 10,
                 'diurne': 960,
             },
             {
@@ -150,17 +141,15 @@ class TestChallenge1:
                 'destination': '48-666666666',
                 'end': 1546394399,
                 'start': 1546308000,
-                'nocturne': 479,
                 'diurne': 960,
             },
         ]
 
         for rec in local_records:
-            noc_min, diu_min = calc_minutes_in_period(rec['start'], rec['end'])
-            assert noc_min == rec['nocturne']
-            assert diu_min == rec['diurne']
+            minutes = calc_diurnal_minutes(rec['start'], rec['end'])
+            assert minutes == rec['diurne']
 
-    def test_fare_value(self):
+    def test_calculate_bills(self):
         local_records = [
             {
                 'source': '48-996355555',
@@ -177,7 +166,6 @@ class TestChallenge1:
                 'total': 2.25,
             }
         ]
-        for rec in local_records:
-            noc_min, diu_min = calc_minutes_in_period(rec['start'], rec['end'])
-            total = calc_fare(diu_min)
-            assert rec['total'] == round(total, 2)
+        bills = calculate_bills(local_records)
+        assert bills[0]['total'] == local_records[1]['total']
+        assert bills[1]['total'] == local_records[0]['total']
